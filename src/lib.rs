62532ccedac5ff788a95fb8034237dc73c244065
akpiya <akashpiya17@gmail.com>
2025-11-02 11:04:34 -0600

    tourny base class skeleton
    (cherry picked from commit 6393eb6dc74b03a9d99b534069163edf47149f7d)

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 use pyo3::prelude::*;
 
-mod hint;
+pub mod hint;
+mod base;
 
 /// Python bindings for wordle-tournament-lib
 #[pymodule]
 fn wordle_tournament_lib(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<hint::WordleHint>()?;
+    m.add_class::<base::UChicagoWordleBotBase>()?;
     Ok(())
 }
