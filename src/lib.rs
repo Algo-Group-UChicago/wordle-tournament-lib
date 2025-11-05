7d61a793d03c2b3df26fd2bac9ad1cb9272e8d06
akpiya <akashpiya17@gmail.com>
2025-11-05 16:51:00 -0600

    functional inheritance and method overloading from UChicagoWordleBotBase

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod base;
 
 /// Python bindings for wordle-tournament-lib
 #[pymodule]
-fn wordle_tournament_lib(_py: Python, m: &PyModule) -> PyResult<()> {
+fn wordle_tournament_lib(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<hint::WordleHint>()?;
     m.add_class::<base::UChicagoWordleBotBase>()?;
     Ok(())
